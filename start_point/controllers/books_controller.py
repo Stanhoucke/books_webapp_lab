96b3deb440e3c1739da4833840e66b47e60b92a8
Stanley Houcke <stanhoucke@gmail.com>
2020-12-09 16:22:39 +0000

    started books/index.html

--- start_point/controllers/books_controller.py
+++ start_point/controllers/books_controller.py
@@ -1,7 +1,23 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from repositories import book_repository
+from repositories import author_repository
+from models.book import Book
+
+# A blueprint is a collection of routes
+books_blueprint = Blueprint("books", __name__)
+
+# INDEX
+@books_blueprint.route('/books')
+def books():
+    # Get all the tasks from the db
+    books = book_repository.select_all()
+    return render_template("books/index.html", all_books=books)
+
 
 # NEW
 # GET '/books/new'
-
+# @route(/books/new)
 # CREATE
 # POST '/books'
 
